87a888d24841cae850efd2dfb7faa9e544162df4
Vinit Bhargava <er.bhargava.vinit@gmail.com>
2020-06-14 11:39:00 +0530

    Add project files.

---
+++ CustomFormsAuthentication/Controllers/AccountsController.cs
@@ -0,0 +1,63 @@
+﻿using CustomFormsAuthentication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace CustomFormsAuthentication.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private CustomFormAuthenticationEntities context = null;
+        // GET: Accounts
+
+        //public AccountsController()
+        //{
+        //    this.db
+        //}
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserModel model)
+        {
+            using (context = new CustomFormAuthenticationEntities())
+            {
+                bool IsValidUser = context.Users.Any(user => user.UserName.ToLower() ==
+                     model.UserName.ToLower() && user.UserPassword == model.UserPassword);
+                if (IsValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+                ModelState.AddModelError("", "invalid Username or Password");
+                return View();
+            }
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(Users model)
+        {
+            using (context = new CustomFormAuthenticationEntities())
+            {
+                context.Users.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
